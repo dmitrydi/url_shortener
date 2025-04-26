234a86bb8686b5e8d22e5bd597ea10eb4fcc3cf1
dmitrydi <d.dmitrook@gmail.com>
2025-04-26 13:52:32 +0300

    iter1: added storage package
    iter1: check storage
    
    iter1: fixed storage logic
    
    iter1: implemented server
    
    iter1: fixed vet issues
    
    iter1: fix vet 2
    
    iter1: fixed put logic

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/dmitrydi/url_shortener/server"
+	"github.com/dmitrydi/url_shortener/storage"
+)
+
+func main() {
+	s := storage.NewBasicStorage("http://localhost:8080/")
+	getHandler := server.MakeGetHandler(s)
+	putHandler := server.MakePutHandler(s)
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/{path}`, getHandler)
+	mux.HandleFunc(`/`, putHandler)
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ storage/storage.go
@@ -0,0 +1,67 @@
+package storage
+
+import (
+	"errors"
+	"math/rand"
+	"strings"
+)
+
+type URLStorage interface {
+	Put(string) (string, error)
+	Get(string) (string, error)
+}
+
+type BasicStorage struct {
+	rootPrefix string
+	data       map[string]string
+}
+
+func NewBasicStorage(rootPrefix string) *BasicStorage {
+	ret := new(BasicStorage)
+	ret.rootPrefix = rootPrefix
+	ret.data = make(map[string]string)
+	return ret
+}
+
+func MakeBasicStorage(rootPrefix string) BasicStorage {
+	var ret BasicStorage
+	ret.rootPrefix = rootPrefix
+	ret.data = make(map[string]string)
+	return ret
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const shortURLLen = 8
+
+func makeRandomString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+func (storage *BasicStorage) Put(intiURL string) (string, error) {
+	var randURL string
+	for {
+		randURL = makeRandomString(shortURLLen)
+		_, ok := storage.data[randURL]
+		if !ok {
+			break
+		}
+	}
+	storage.data[randURL] = intiURL
+	return storage.rootPrefix + randURL, nil
+}
+
+func (storage *BasicStorage) Get(shortURL string) (string, error) {
+	val, ok := storage.data[shortURL]
+	if !ok {
+		return "", errors.New("url not exists")
+	}
+	return val, nil
+}
+
+func (storage *BasicStorage) RemovePrefix(url string) string {
+	return strings.TrimPrefix(url, storage.rootPrefix)
+}
