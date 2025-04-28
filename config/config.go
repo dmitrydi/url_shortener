b8b9c59ec4705d6d7c9f76251e6f2c2dbef2a858
dmitrydi <d.dmitrook@gmail.com>
2025-04-28 20:35:01 +0300

    iter5: added os
    iter5: fixed vet issues
    
    iter5: added default params

--- config/config.go
+++ config/config.go
@@ -2,9 +2,28 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
+const serverEnv = "SERVER_ADDRESS"
+const baseURLEnv = "BASE_URL"
+const defaultServerAddr = ":8080"
+const defaultBaseURL = "http://localhost:8080/"
+
 var (
-	ServerAddr = flag.String("a", ":8080", "address of server")
-	URLPrefix  = flag.String("b", "http://localhost:8080/", "short URL prefix")
+	ServerAddr *string
+	URLPrefix  *string
 )
+
+func init() {
+	srv, ok := os.LookupEnv(serverEnv)
+	if !ok {
+		srv = defaultServerAddr
+	}
+	base, ok := os.LookupEnv(baseURLEnv)
+	if !ok {
+		base = defaultBaseURL
+	}
+	ServerAddr = flag.String("a", srv, "address of server")
+	URLPrefix = flag.String("b", base, "short URL prefix")
+}
