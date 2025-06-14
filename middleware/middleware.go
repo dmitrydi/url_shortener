3b88d0fa8408928d077c07301aefd64e1af4733f
dmitrydi <d.dmitrook@gmail.com>
2025-06-14 19:24:20 +0300

    Revert "iter14: changed reading"
    This reverts commit a9e64e3c307a429bc14efc447271ed682681e2e5.

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -77,9 +77,7 @@ func MakeDecompressHandler() func(http.Handler) http.Handler {
 				return
 			}
 			defer gz.Close()
-			//decompBody, err := io.ReadAll(gz)
-			var decompBody []byte
-			_, err = gz.Read(decompBody)
+			decompBody, err := io.ReadAll(gz)
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
 				return
@@ -93,3 +91,18 @@ func MakeDecompressHandler() func(http.Handler) http.Handler {
 		return http.HandlerFunc(fn)
 	}
 }
+
+/*
+func MakeCompressHandler(pl *sync.Pool) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		compFn := func(w http.ResponseWriter, r *http.Request) {
+			gz := pl.Get().(*gzip.Writer)
+			gz.Reset(w)
+			defer gz.Close()
+			cw := customWriter{ResponseWriter: w, Writer: gz, Compress: strings.Contains(r.Header.Get("Accept-Encoding"), "gzip"), StatusCode: 0}
+			h.ServeHTTP(&cw, r)
+		}
+		return http.HandlerFunc(compFn)
+	}
+}
+*/
