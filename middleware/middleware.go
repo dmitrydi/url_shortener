111e95eda404557e700fe18e607786e831ba5d57
dmitrydi <d.dmitrook@gmail.com>
2025-06-14 18:20:29 +0300

    iter14: removed comments

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,8 +3,6 @@ package middleware
 import (
 	"compress/gzip"
 	"net/http"
-
-	"github.com/google/uuid"
 )
 
 type DecompReader struct {
@@ -38,26 +36,3 @@ func MakeDecompReader(r *http.Request) (*DecompReader, error) {
 	}
 	return cr, nil
 }
-
-type AuthedHandler func(w http.ResponseWriter, r *http.Request, userID uuid.UUID)
-
-func AuthMiddleware(next AuthedHandler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
-}
-
-/*
-
-func MakeCompressHandler(pl *sync.Pool) func(http.Handler) http.Handler {
-	return func(h http.Handler) http.Handler {
-		compFn := func(w http.ResponseWriter, r *http.Request) {
-			gz := pl.Get().(*gzip.Writer)
-			gz.Reset(w)
-			defer gz.Close()
-			cw := customWriter{ResponseWriter: w, Writer: gz, Compress: strings.Contains(r.Header.Get("Accept-Encoding"), "gzip"), StatusCode: 0}
-			h.ServeHTTP(&cw, r)
-		}
-		return http.HandlerFunc(compFn)
-	}
-}
-
-*/
