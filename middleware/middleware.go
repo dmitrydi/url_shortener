4212f671079958f6c02ec28e1f191b8d89cb64f3
dmitrydi <d.dmitrook@gmail.com>
2025-06-14 19:24:03 +0300

    Revert "iter14: changed to string body"
    This reverts commit d34781a5de0514012ca66009c56d1c5e3d97daa9.

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -5,7 +5,6 @@ import (
 	"compress/gzip"
 	"io"
 	"net/http"
-	"strings"
 )
 
 type DecompReader struct {
@@ -85,10 +84,9 @@ func MakeDecompressHandler() func(http.Handler) http.Handler {
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
-			sBody := string(decompBody)
 			newReq := r.Clone(r.Context())
-			newReq.Body = io.NopCloser(strings.NewReader(sBody))
-			newReq.ContentLength = int64(len(sBody))
+			newReq.Body = io.NopCloser(bytes.NewReader(decompBody))
+			newReq.ContentLength = int64(len(decompBody))
 			newReq.Header.Del("Content-Encoding")
 			next.ServeHTTP(w, newReq)
 		}
